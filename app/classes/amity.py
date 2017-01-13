4b44ae232f1adc40d0cb776e252755830ea87d32
andela-ik <ian.kingori@andela.com>
2017-01-13 12:39:54 +0300

    Implemented Function: Add Person, Allocate Room

--- app/classes/amity.py
+++ app/classes/amity.py
@@ -18,7 +18,58 @@ class Amity():
         wants_accomodation -- Y|N Does the person require a living space(Fellows Only)
         """
 
-        return
+        for person in self.people:
+            if name.upper() == person.name:
+                print(person.name + " ALREADY EXISTS")
+                return
+        if role.upper() == 'STAFF':
+            if wants_accomodation == 'Y':
+                print("Error, Staff cannot be allocated a living space")
+                return "Error, Staff cannot be allocated a living space"
+            office = self.select_random_room(Office)
+            person = Staff(name, office )
+            self.people.append(person)
+            if office is not None:
+                self.allocate_room(person, office)
+            else:
+                self.office_unallocated.append(person)
+
+            print(person.name + " added successfully")
+
+        elif role.upper() == 'FELLOW':
+            lspace = None
+            office = None
+            if wants_accomodation == 'Y':
+                lspace = self.select_random_room(Lspace)
+            office = self.select_random_room(Office)
+            person = Fellow(name, office, lspace)
+            self.people.append(person)
+            if office is not None:
+                self.allocate_room(person, office)
+            else:
+                self.office_unallocated.append(person)
+
+            if lspace is not None:
+                self.allocate_room(person, lspace)
+            elif wants_accomodation == 'Y' and lspace is None:
+                self.lspace_unallocated.append(person)
+            else:
+                pass
+
+            print(person.name + " added successfully")
+
+        else:
+            person = "Invalid role"
+
+        return person
+
+
+    def allocate_room(self, person, room):
+        """ Allocates a person to a specific room"""
+        if room is not None and room.check_availability():
+            room.occupants.append(person.name.upper())
+            room.number_of_occupants += 1
+
 
 
     def create_room(self, name, room_type):
@@ -50,6 +101,8 @@ class Amity():
         print(message)
         return room
 
+        
+    #TODO: Implement Automated Waiting List
     def on_room_update(self):
         """ Track vacancies and auto allocate unallocated people"""
         return
@@ -87,6 +140,15 @@ class Amity():
         """
         return
 
+    def save_state(self, db_name = "default"):
+        """ Store all data in memory to database
+
+        Keyword arguments:
+        db_name -- if specified, save to specified database, else use default
+        """
+        return
+
+
     def search_person(self, person_name):
         """ Check if person exists in the system, return Person instance or False"""
         return
@@ -98,13 +160,23 @@ class Amity():
         """ Check if room exists in the system, return Room instance or False"""
         return
 
-    def save_state(self, db_name = "default"):
-        """ Store all data in memory to database
 
-        Keyword arguments:
-        db_name -- if specified, save to specified database, else use default
-        """
-        return
+
+    def select_random_room(self, room_type):
+        """ Given a room type, selects a vacant room at random"""
+        available_rooms = []
+        selected_room = None
+        for room in self.rooms:
+            if type(room) == room_type:
+                if(room.check_availability()):
+                    available_rooms.append(room)
+        number_available = len(available_rooms)
+        if number_available > 0:
+            selected_room = random.sample(available_rooms, 1)[0]
+        else:
+            #TODO:Implement waiting list
+            print("There are no vacancies")
+        return selected_room
 
 
 
